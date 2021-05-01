086a368bfde6a157d1172169e00a4d035ad6dfa0
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-05-01 13:43:36 +0900

    init

---
+++ 00_HelloWorld/cmake-build-debug/CMakeFiles/00_HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "00_HelloWorld"
+  "00_HelloWorld.pdb"
+  "CMakeFiles/00_HelloWorld.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/00_HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01_InputOutput/cmake-build-debug/CMakeFiles/01_InputOutput.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_InputOutput"
+  "01_InputOutput.pdb"
+  "CMakeFiles/01_InputOutput.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_InputOutput.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
