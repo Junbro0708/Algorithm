3efc1afdd5e2a5a322d345fae997595cf74b1061
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-18 17:47:14 +0900

    프로그래머스 소수 만들기

---
+++ 11_PrimeNumber/cmake-build-debug/CMakeFiles/11_PrimeNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11_PrimeNumber"
+  "11_PrimeNumber.pdb"
+  "CMakeFiles/11_PrimeNumber.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_PrimeNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11_PrimeNumber/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+/*
+ * 프로그래머스 소수 만들기
+ */
+
+using namespace std;
+
+bool isPrime(int num){
+    for(int i = 2; i < num; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0, num;
+
+    for(int i = 0; i < nums.size() - 2; i++){
+        for(int j = i + 1; j < nums.size() - 1; j++){
+            for(int z = j + 1; z < nums.size(); z++){
+                num = nums[i] + nums[j] + nums[z];
+                if(isPrime(num)) answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> num = {1, 2, 3, 4};
+    cout << solution(num) << endl;
+    return 0;
+}
