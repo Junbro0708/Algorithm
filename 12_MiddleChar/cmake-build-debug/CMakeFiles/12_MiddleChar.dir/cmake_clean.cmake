de949e2337007fc0c5f2429dd269996018ab46ce
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-18 18:27:40 +0900

    프로그래머스 가운데 글자 가져오기

---
+++ 12_MiddleChar/cmake-build-debug/CMakeFiles/12_MiddleChar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "12_MiddleChar"
+  "12_MiddleChar.pdb"
+  "CMakeFiles/12_MiddleChar.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_MiddleChar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12_MiddleChar/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+/*
+ * 프로그래머스 가운데 글자 가져오기
+ */
+
+using namespace std;
+
+string solution(string str) {
+    string answer = "";
+
+    if(str.length() % 2 == 1)
+        answer = str[str.length() / 2];
+    else{
+        answer = str[str.length() / 2 - 1];
+        answer += str[str.length() / 2];
+    }
+    return answer;
+}
+
+int main() {
+    cout << solution("qwer");
+
+    return 0;
+}
