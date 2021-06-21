66b6fc4e872a62570b7c887665b7be489362ffc2
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-21 18:07:39 +0900

    백준 1138번 한 줄로 서기 -ing

---
+++ 14_OneLine/cmake-build-debug/CMakeFiles/14_OneLine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "14_OneLine"
+  "14_OneLine.pdb"
+  "CMakeFiles/14_OneLine.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14_OneLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14_OneLine/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+/*
+ *  백준 1138번 한 줄로 서기
+ */
+
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+    vector<int> line(N);
+
+    for(int i = 0; i < N; i++){
+        int temp;
+        cin >> temp;
+        for(int j = 0; j < N; j++){
+            if(temp == 0 && line[j] == 0){
+                line[j] = i + 1;
+            }else{
+                temp--;
+            }
+        }
+    }
+
+    for(int k = 0; k < N; k++){
+        cout << line[k];
+    }
+}
