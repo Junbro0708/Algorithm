119793c5a5f2627acb7cc00edf4cc23c31355783
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-22 18:37:15 +0900

    프로그래머스 2016

---
+++ 16_2016/cmake-build-debug/CMakeFiles/16_2016.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "16_2016"
+  "16_2016.pdb"
+  "CMakeFiles/16_2016.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16_2016.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
