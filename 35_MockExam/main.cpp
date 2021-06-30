87c182d67e66476602ba7c7cd44640d44f1984a7
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-30 18:52:13 +0900

    [35] 프로그래머스 모의고사

--- 35_MockExam/main.cpp
+++ 35_MockExam/main.cpp
@@ -10,19 +10,17 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
+    vector<int> score(3);
     vector<int> arr1 = {1, 2, 3, 4, 5};
     vector<int> arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-    int n1 = 0, n2 = 0, n3 = 0;
-
-    for(int i = 0; i < answer.size(); i++){
-        if(arr1[i % 5] == answer[i]) n1++;
-        if(arr2[i % 8] == answer[i]) n2++;
-        if(arr3[i % 10] == answer[i]) n3++;
+    for(int i = 0; i < answers.size(); i++){
+        if(arr1[i % 5] == answers[i]) score[0]++;
+        if(arr2[i % 8] == answers[i]) score[1]++;;
+        if(arr3[i % 10] == answers[i]) score[2]++;
     }
 
-    vector<int> score = {n1, n2, n3};
     int max = *max_element(score.begin(), score.end());
 
     for(int i = 0; i < score.size(); i++){
@@ -32,6 +30,6 @@ vector<int> solution(vector<int> answers) {
     return answer;
 }
 
-int main() { // test
+int main() {
     solution({1, 2, 3, 4, 5});
 }
