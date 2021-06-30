5d7c5c6a8954092bc9e58c2d60a76b4b929eb9e4
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-30 16:17:38 +0900

    프로그래머스 예산

---
+++ 33_Budget/cmake-build-debug/CMakeFiles/33_Budget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "33_Budget"
+  "33_Budget.pdb"
+  "CMakeFiles/33_Budget.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/33_Budget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 33_Budget/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+/*
+ * 프로그래머스 예산
+ */
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int cnt = 0;
+    sort(d.begin(), d.end());
+    for(int i = 0; i < d.size(); i++){
+        if(d[i] <= budget){
+            budget -= d[i];
+            cnt++;
+        }else{
+            break;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    std::cout << solution({1, 3, 2, 5, 4}, 9) << std::endl;
+    return 0;
+}
