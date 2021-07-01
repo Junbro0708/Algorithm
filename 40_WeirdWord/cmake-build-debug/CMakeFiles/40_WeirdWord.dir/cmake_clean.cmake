fe35f65a4a7b406b03bc0d0efc6f705f856ecaed
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-07-01 19:02:16 +0900

    [40] 프로그래머스 이상한 문자 만들기

---
+++ 40_WeirdWord/cmake-build-debug/CMakeFiles/40_WeirdWord.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "40_WeirdWord"
+  "40_WeirdWord.pdb"
+  "CMakeFiles/40_WeirdWord.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/40_WeirdWord.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
