d62c5fcde4b6e0d55d015023f29a48037f3c9806
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-07-17 15:30:39 +0900

    [71] 프로그래머스 H-Index

--- 71_H_Index/main.cpp
+++ 71_H_Index/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
 
 /*
  * [71] 프로그래머스 H-Index
@@ -10,6 +10,12 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for(int i = 0; i < citations.size(); i++){
+        if(citations[i] <= answer) break;
+        answer++;
+    }
     return answer;
 }
 
