2bd4e21c5c37f220c60161ec5e67b3f03646e80c
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-07-22 16:43:35 +0900

    [84] 프로그래머스 방문 길이

---
+++ 84_TheFirstRoad/cmake-build-debug/CMakeFiles/84_TheFirstRoad.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "84_TheFirstRoad"
+  "84_TheFirstRoad.pdb"
+  "CMakeFiles/84_TheFirstRoad.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/84_TheFirstRoad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
