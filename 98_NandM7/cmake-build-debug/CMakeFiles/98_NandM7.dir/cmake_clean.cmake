aff1ac05fad583f82484fab5ff260090a8099052
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-07-28 22:43:37 +0900

    [98] 백준 15656번 N과 M (7)

---
+++ 98_NandM7/cmake-build-debug/CMakeFiles/98_NandM7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "98_NandM7"
+  "98_NandM7.pdb"
+  "CMakeFiles/98_NandM7.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/98_NandM7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
