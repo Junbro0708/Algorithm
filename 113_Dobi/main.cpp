cabebc4a52a179360f831697649d203c51407d84
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-07 15:47:27 +0900

    [113] 백준 2204번 도비의 난독증 테스트

--- 113_Dobi/main.cpp
+++ 113_Dobi/main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+
+/*
+ * [113] 백준 2204번 도비의 난독증 테스트
+ */
 
 using namespace std;
 
-int main() {
-    int isPlay = 1;
-
-    while(isPlay){
-        cin >> isPlay;
-        vector<string> arr;
-
-        for(int i = 0; i < isPlay; i++){
-            string temp;
-            cin >> temp;
-            arr.push_back(temp);
+bool fast(string a, string b) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] >= 'a' && a[i] <= 'z') {
+            a[i] -= 32;
         }
-
     }
+    for (int j = 0; j < b.size(); j++) {
+        if (b[j] >= 'a' && b[j] <= 'z') {
+            b[j] -= 32;
+        }
+    }
+    if (a.compare(b) > 0) return false;
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout.tie(NULL); cin.tie(NULL);
+
+    int N; cin >> N;
+
+    while (N != 0) {
+        vector<string> v(N);
+        for (int i = 0; i < N; i++) {
+            string s;
+            cin >> s;
+            v[i] = s;
+        }
+        sort(v.begin(), v.end(), fast);
+        cout << v[0] << "\n";
+        cin >> N;
+    }
+
     return 0;
 }
