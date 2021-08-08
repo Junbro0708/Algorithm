51aed7c8b6485ad2ecf59c50013f73b6161c0ca1
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-08 16:49:35 +0900

    [120] 백준 1158번 요세푸스 문제

---
+++ 120_yosefus/cmake-build-debug/CMakeFiles/120_yosefus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "120_yosefus"
+  "120_yosefus.pdb"
+  "CMakeFiles/120_yosefus.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/120_yosefus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
