59f3aef8d07bfd9f48da2ccdcea8a4e81a21fa8a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-15 20:34:58 +0900

    [131] 백준 1431번 시리얼 번호

---
+++ 131_SerialNumber/cmake-build-debug/CMakeFiles/131_SerialNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "131_SerialNumber"
+  "131_SerialNumber.pdb"
+  "CMakeFiles/131_SerialNumber.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/131_SerialNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
