e89a603ffab5aa2a4d3c61570822c7f6cf52f7d8
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-17 17:08:52 +0900

    [136] 백준 11723번 집합

---
+++ 136_Shape/cmake-build-debug/CMakeFiles/136_Shape.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "136_Shape"
+  "136_Shape.pdb"
+  "CMakeFiles/136_Shape.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/136_Shape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 136_Shape/main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+/*
+ * [136] 백준 11723번 집합
+ */
+
+using namespace std;
+
+int N;
+vector<pair<pair<int, int>, int>> V[50];
+
+void Input()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        int a, b;
+        cin >> a >> b;
+        V[i].push_back(make_pair(make_pair(a, b), 1));
+    }
+}
+
+void Solution(){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (i == j) continue;
+            if (V[i].front().first.first < V[j].front().first.first && V[i].front().first.second < V[j].front().first.second)
+                V[i].front().second++;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+        cout << V[i].front().second << " ";
+}
+
+int main(void){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    Input();
+    Solution();
+    return 0;
+}
