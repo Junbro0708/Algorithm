0c7ee6bc9a1dc4a97075cd8f1ec8898a0df62056
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-18 16:09:14 +0900

    [139] 백준 4796번 캠핑

---
+++ 139_Camping/cmake-build-debug/CMakeFiles/139_Camping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "139_Camping"
+  "139_Camping.pdb"
+  "CMakeFiles/139_Camping.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/139_Camping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
