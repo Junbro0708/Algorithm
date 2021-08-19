789bd9e89f968c82e4ca84e4431fa7c2bb5825d1
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-19 17:45:38 +0900

    [143] 백준 4673번 셀프 넘버

---
+++ 143_SelfNumber/cmake-build-debug/CMakeFiles/143_SelfNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "143_SelfNumber"
+  "143_SelfNumber.pdb"
+  "CMakeFiles/143_SelfNumber.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/143_SelfNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 143_SelfNumber/cmake-build-debug/CMakeFiles/143_SelfNumber.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 143_SelfNumber.
+# This may be replaced when dependencies are built.
