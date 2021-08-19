ca40a14a2cd8780aeadcb48bf5e1872e83070721
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-19 17:31:27 +0900

    [142] 백준 1343번 폴리오미노

---
+++ 142_Poliomino/cmake-build-debug/CMakeFiles/142_Poliomino.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "142_Poliomino"
+  "142_Poliomino.pdb"
+  "CMakeFiles/142_Poliomino.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/142_Poliomino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
