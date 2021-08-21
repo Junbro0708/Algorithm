be15a7558b82d6438e237ea6455b92ddf453147e
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-21 16:46:10 +0900

    [155] 백준 2435번 기상청 인턴 신현수

--- 155_Weather/main.cpp
+++ 155_Weather/main.cpp
@@ -16,10 +16,10 @@ int main() {
     for(int i = 0; i < N; i++)
         cin >> arr[i];
 
-    for(int i = 0; i < N - K; i++){
+    for(int i = 0; i <= N - K; i++){
         int num = arr[i];
         int k = K;
-        for(int j = i + 1; k < 1; j++){
+        for(int j = i + 1; k > 1; j++){
             num += arr[j];
             k--;
         }
