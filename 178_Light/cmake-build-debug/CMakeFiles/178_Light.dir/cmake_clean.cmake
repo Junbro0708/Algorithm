28c0a688091c6a508b6e413de04414a127cb4b4a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-27 17:53:08 +0900

    [178] 백준 2485번 가로수

---
+++ 178_Light/cmake-build-debug/CMakeFiles/178_Light.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "178_Light"
+  "178_Light.pdb"
+  "CMakeFiles/178_Light.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/178_Light.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 178_Light/cmake-build-debug/CMakeFiles/178_Light.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 178_Light.
+# This may be replaced when dependencies are built.
