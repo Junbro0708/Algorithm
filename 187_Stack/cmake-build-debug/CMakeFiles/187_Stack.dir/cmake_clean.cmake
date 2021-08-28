9b3c920c9b758c05a0175f5a0252461e3d76b7f9
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-28 21:34:06 +0900

    [187] 백준 10828번 스택

---
+++ 187_Stack/cmake-build-debug/CMakeFiles/187_Stack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "187_Stack"
+  "187_Stack.pdb"
+  "CMakeFiles/187_Stack.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/187_Stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
