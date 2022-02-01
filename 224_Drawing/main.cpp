f52c8de48a4ddce9780a5310d8d4b0a65e26ee88
Junbro0708 <qkrwns708@soongsil.ac.kr>
2022-02-01 21:21:30 +0900

    DFS using Reculsive, Stack

---
+++ 224_Drawing/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+
+/*
+ * [224] 백준 1926번 그림
+ */
+
+using namespace std;
+
+#define MAX_N = 500
+
+struct Point{
+   int row, col;
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- algorithm_DFS/main.cpp
+++ algorithm_DFS/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <stack>
 
 using namespace std;
 
@@ -7,6 +8,46 @@ using namespace std;
 int N, E;
 int Graph[MAX_N][MAX_N];
 bool Visited[MAX_N];
+
+/*
+ * 예시
+ * 입력 5 6
+ *     0 1 0 2 1 3 1 4 2 4 3 4
+ */
+
+void DFS_reculsive(int node){
+    Visited[node] = true;
+    cout << node << " ";
+
+    for(int next = 0; next < N; ++next){
+        if(!Visited[next] && Graph[node][next]){
+            DFS_reculsive(next);
+        }
+    }
+}
+
+void DFS_stack(int node){
+    bool Visited_s[MAX_N] = {false, };
+    stack<int> myStack;
+
+    myStack.push(node);
+
+    while (!myStack.empty()){
+        int currentNode = myStack.top();
+        myStack.pop();
+
+        if(!Visited_s[currentNode])
+            continue;
+
+        Visited_s[currentNode] = true;
+        cout << currentNode << " ";
+
+        for(int next = 0; next < N; ++next){
+            if(!Visited_s[next] && Graph[currentNode][next])
+                myStack.push(next);
+        }
+    }
+}
 
 int main() {
     cin >> N >> E;
@@ -18,6 +59,7 @@ int main() {
         cin >> u >> v;
         Graph[u][v] = Graph[v][u] = 1; // 노드를 만들때 간선 정보를 담는 법
     }
-    DFS(0);
+    DFS_reculsive(0);
+    DFS_stack(0);
     return 0;
 }
