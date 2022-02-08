c149502d29db37c4417fe2c555b448fe9815b64a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2022-02-08 15:37:32 +0900

    [231] 백준 단지 번호 붙이기

--- 231_Danji/main.cpp
+++ 231_Danji/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <cstring>
 #include <queue>
+#include <vector>
+#include <algorithm>
 
 /*
  * [231] 백준 단지 번호 붙이기
@@ -8,28 +9,62 @@
 
 using namespace std;
 
+#define MAX 25
+
 int N;
-int D[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int Board[MAX][MAX];
+bool Visited[MAX][MAX] = {false, };
+
+int D[4][2] = {{-1, 0},
+               {0, 1},
+               {1, 0},
+               {0, -1}};
 
 void BFS(){
-    bool Visited[N][N];
-    memset(Visited, false, sizeof(Visited[N][N]));
-
     queue<pair<pair<int, int>, int>> myQueue;
+    vector<int> danji;
+    int total = 0;
 
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < N; ++j){
-            if(B)
+            if(Board[i][j] == 1 && !Visited[i][j]){
+                total++;
+
+                int cnt = 0;
+                myQueue.push({{i, j}, cnt});
+                Visited[i][j] = true;
+
+                while (!myQueue.empty()){
+                    auto t = myQueue.front();
+                    myQueue.pop();
+
+                    auto tPos = t.first;
+                    int tx = tPos.first; int ty = tPos.second;
+                    cnt++;
+
+                    for(int i = 0; i < 4; ++i){
+                        int dx = tx + D[i][0];
+                        int dy = ty + D[i][1];
+
+                        if(dx < 0 || dy < 0 || dx >= N || dy >= N) continue;
+                        if(Board[dx][dy] == 0) continue;
+                        if(Visited[dx][dy]) continue;
+                        myQueue.push({{dx, dy}, cnt});
+                        Visited[dx][dy] = true;
+                    }
+                }
+                danji.push_back(cnt);
+            }
         }
     }
+    cout << total << "\n";
+    sort(danji.begin(), danji.end());
+    for(int i = 0; i < danji.size(); ++i)
+        cout << danji[i] << "\n";
 }
 
 int main() {
     cin >> N;
-
-    int Board[N][N];
-    memset(Board, 0, sizeof(Board[N][N]));
-
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < N; ++j){
             scanf_s("%1d", &Board[i][j]);
